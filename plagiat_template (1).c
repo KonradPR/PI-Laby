a71bfaf06d9a1fdc7f3a38af8e3d6636e041ec3a
KonradPR <32310362+KonradPR@users.noreply.github.com>
2019-05-16 23:26:07 +0200

    Update plagiat_template (1).c

--- plagiat_template (1).c
+++ plagiat_template (1).c
@@ -5,7 +5,50 @@
 int MAX_TEXT_LENGTH = 200;
 
 
-
+int minimum(int x1, int x2, int x3)
+{
+    if (x1 >= x2)
+        if (x2 >= x3)
+            return x3;
+        else
+            return x2;
+    else
+        if (x3 >= x1)
+            return x1;
+ 
+    return x3;
+}
+ 
+ 
+int MAX_TEXT_LENGTH = 200;
+ 
+int distance(char* a, int a_length, char* b, int b_length) {
+    /*********************
+    * put your code here *
+    *********************/
+    int** check=(int**)malloc(sizeof(int*)*MAX_TEXT_LENGTH+1);
+    for(int i=0;i<MAX_TEXT_LENGTH+1;i++)
+        check[i]=(int*)malloc(sizeof(int)*MAX_TEXT_LENGTH+1);\
+    int m=a_length;
+    int n=b_length;
+    for(int i=0;i<=m;i++)   check[i][0]=i;
+    for(int i=0;i<=n;i++)   check[0][i]=i;
+    int cost;
+    for(int i=1;i<=m;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+           
+            if(a[i-1]==b[j-1])
+                cost=0;
+            else
+                cost=1;
+            check[i][j]=minimum(check[i-1][j]+1,check[i][j-1]+1,check[i-1][j-1]+cost);
+        }
+    }
+    return check[a_length][b_length];
+}
+ 
 
 int distance(char* a, int a_length, char* b, int b_length) {
 
